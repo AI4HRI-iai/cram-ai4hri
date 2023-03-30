4c5f02d3fbc7ef1299dd564f5fbb45e92a973360
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2023-03-30 11:39:01 +0200

    Add dialog sentence splitter and switch

--- cram_demos/cram_pr2_pick_place_demo/src/ease-hackathon.lisp
+++ cram_demos/cram_pr2_pick_place_demo/src/ease-hackathon.lisp
@@ -1,9 +1,7 @@
 (in-package :demo)
 
 (defparameter *objects-list* '(:bowl :spoon :cup :milk :breakfast-cereal))
-
 (defparameter *dialog-subscriber* nil)
-
 (defparameter *dialog-fluent* (cpl:make-fluent :name :dialog-fluent :value nil))
 
 (defun interaction-demo ()
@@ -12,22 +10,36 @@
     ;; (unless (eq (roslisp:node-status) :running)
     ;;   (roslisp-utilities:startup-ros))
     (initialize)
-    (setf *dialog-subscriber* (roslisp:subscribe "dialog" "std_msgs/String" #'set-dialog-context))))
+    (setf *dialog-subscriber*
+          (roslisp:subscribe "dialog" "std_msgs/String" #'dialog-listener-callback))))
 
-(defun set-dialog-context (message)
+(defun dialog-listener-callback (message)
   (setf *dialog-fluent* message)
-  (greet message))
+  (urdf-proj:with-simulated-robot
+    (apply #'greet (message->dialog-parameters message))))
 
-(defun greet (message)
+(defun message->dialog-parameters (message)
   (roslisp:with-fields (data) message
-    (urdf-proj:with-simulated-robot
-      (let ((arm (if (string= data "right")
-                     :right
-                     (if (string= data "left")
-                         :left
-                         nil))))
-        (greeting :gripper arm)))))
+    (remove :|| (mapcar (lambda (word)
+                          (intern (string-upcase word) :keyword))
+                        (split-sequence:split-sequence '#\space data)))))
 
+(defun greet (&rest params)
+  (print (format t "Received: ~a" params))
+  (case (first params)
+    (:WAVING (if (member (second params) '(:right :left))
+                 (greeting :gripper (second params))
+                 (greeting)))
+    (:POINTING (if (member (third params)
+                           (mapcar #'btr:name
+                                   (btr:objects btr:*current-bullet-world*)))
+                   (looking-at :object-name (third params))
+                   (print (format t "Third word is ~a but expected any of ~a"
+                                  (third params)
+                                  (mapcar #'btr:name
+                                          (btr:objects btr:*current-bullet-world*))))))
+    (:NOTHING (print "Nothing to be done"))
+    (otherwise (print "No data received"))))
 
 (defun greeting (&key (gripper :right) ;; right or left
                       (day-time "morning"))
@@ -48,3 +60,21 @@
              (type positioning-arm)
              (left-configuration tucked)
              (right-configuration tucked))))
+
+(defun looking-at (&key object-name)
+  (print (format T "Pointing at ~a" object-name))
+  (print "Not implemented yet."))
+  
+
+;; (defun greet (message)
+;;   (urdf-proj:with-simulated-robot
+;;       ;; (split-sequence:split-sequence '#\space data)
+;;       (let ((arm (if (string= data "right")
+;;                      :right
+;;                      (if (string= data "left")
+;;                          :left
+;;                          nil))))
+;;         (greeting :gripper arm)))))
+
+
+
